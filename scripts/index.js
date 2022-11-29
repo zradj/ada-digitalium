baecaf314ac95d6336d1018c1224fe4e318a2d0c
zradj <zradj7@gmail.com>
2022-11-30 02:35:32 +0400

    create 'back-to-top' button

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,25 @@
 const onScroll = () => {
   const navbar = document.getElementById('navbar');
-  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 50) {
+  const backToTopButton = document.getElementById('back-to-top');
+  const main = document.querySelector('main');
+
+  if (main.scrollTop > 75) {
     navbar.style.maxHeight = '60px';
     navbar.querySelector('.navbar-logo').style.maxHeight = '50px';
+    backToTopButton.style.transform = 'translate(0)';
   } else {
     navbar.style.maxHeight = '75px';
     navbar.querySelector('.navbar-logo').style.maxHeight = '60px';
+    backToTopButton.style.transform = 'translate(0, 200%)';
   }
 };
 
-window.onscroll = onScroll;
+const onClickBackToTop = () => (document.querySelector('main').scrollTop = 0);
+
+const onDOMContentLoaded = () => {
+  onScroll();
+  document.querySelector('main').onscroll = onScroll;
+  document.getElementById('back-to-top').addEventListener('click', onClickBackToTop);
+};
+
+addEventListener('DOMContentLoaded', onDOMContentLoaded);
